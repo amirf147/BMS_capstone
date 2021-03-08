64208118e51373bff6573fb9c35a7be9191aafcd
Amir Farhadi <amirf147@gmail.com>
2021-03-08 16:53:41 +0200

    further consolidated code into module

--- graph_csv.py
+++ graph_csv.py
@@ -1,39 +1,19 @@
-# import your desired settings from the settings module
 import pandas as pd
-from settings import sixcells as sixc
-from settings import discharge_data as dc_df
+from settings import Dataframe
+from settings import Plot
+
+# Remove excess datapoints and tidy up the dataframe, the first parameter is
+# the file name and the second parameter is the nth number of lines to save
+dc_df = Dataframe('discharge_2cells_clean.csv', 400)
 
 df = dc_df.clean() # initial set up of pandas dataframe
+dc_df.add_time_column(df, 'h') # add a column with hours elapsed
 
-def add_time_column(data_frame, hms):
-    '''call with dataframe object and specify if want time in hours minutes or
-    seconds by passing 'h', 'm' or 's' to hms parameter'''
-
-    times_list = [] #holder for the conversion of elapsed time to time in hours
-    A, B, C = 0, 0, 0 #Time conversion coefficients
-    
-    if hms == 'h':
-        A, B, C = 1, 1 / 60, 1 / 3600
-    elif hms == 'm':
-        A, B, C = 60 , 1, 1 / 60
-    elif hms == 's':
-        A, B, C = 3600, 60, 1
-    else:
-        return 'Error: hms should be string "h" or "m" or "s"'
-    
-    for row_number in range(len(df)):
-        times_list.append(df.loc[row_number, 'Elapsed Time'])
-        hours = int(times_list[row_number][3])
-        minutes = int(times_list[row_number][5:7])
-        seconds = int(times_list[row_number][8:10])
-        times_list[row_number] = (hours * A) + (minutes * B) + (seconds * C)
-
-    hours_column = pd.Series(times_list)
-    df.insert(loc = 0, column = 'Time (hours)', value = hours_column)
-    return df
-
-df = add_time_column(df, 'h')
+# Choose which columns to be graphed
+graph_6cells = Plot('Cell Voltages: Discharge on Cell 3 & Cell 5',
+                 'Time (hours)', ['Cell 1', 'Cell 2', 'Cell 3',
+                                  'Cell 4', 'Cell 5', 'Cell 6'])
 
 # Apply plot setting and show plot
-sixc.set_options(df, show_plot = True)
+graph_6cells.set_options(df, show_plot = True) 
 
--- settings.py
+++ settings.py
@@ -4,22 +4,71 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
     print("I prefer to be a module.")
 
+
+
 class Csv:
 
-    path = 'datalogs\\raw\\'
+    rawfiles_path = 'datalogs\\raw\\'
+    cleanfiles_path = 'datalogs\\cleaned\\'
     column_names = []
     
     def __init__(self, file_name):
         self.file_name = file_name
 
     def to_lines(self):
-        a_file = open(self.path + self.file_name, "r")
+
+        a_file = open(self.rawfiles_path + self.file_name, "r")
         lines = a_file.readlines()
         a_file.close()
         self.column_names = lines[4] # holds the column names of the table
-        lines = lines[6:-4] # remove first six lines and last 4 lines w/ no data
+        lines = lines[6:-4] # remove first 6 and last 4 lines w/ no data
         return lines
+
+    def to_chars(self, lines):
+        # Create a list of lists of each character
+        divided_lines = [] # holder for list of lists 
+        char_list = [] # used to temporarily hold chars in each line
+        
+        for line_num in range(len(lines)):
+            for char in lines[line_num]:   
+                char_list.append(char)
     
+            divided_lines.append(char_list)
+            char_list = []
+        return divided_lines
+
+    def fix_decimals(self, divided_lines):
+        # Change X,XXXX to X.XXXX in data
+        comma_count = 0 # num of commas tracker
+        for line in range(len(divided_lines)):
+            for char in range(len(divided_lines[line])):
+                if divided_lines[line][char] == ',':
+                    comma_count += 1
+                    if ((comma_count >= 10) and (comma_count % 2 == 0)):
+            #this if statement is to do with the fact that the first decimal
+            #point comes from the 10th occurence of a comma in the line and
+            #then every other instance thereafter until the end of the line
+                        divided_lines[line][char] = '.'
+                
+            comma_count = 0
+        # Recreate the list of lines
+        line = ''
+        lines = []
+        for line_num in range(len(divided_lines)):
+            for char in range(len(divided_lines[line_num])):
+                line += divided_lines[line_num][char]
+            lines.append(line)
+            line = ''
+        return lines
+
+    def write_to_file(self, lines, new_file_suffix = '_clean.csv'):
+        f = open(self.cleanfiles_path + self.file_name[:-4] + new_file_suffix,
+                 'w+')
+        f.write(self.column_names) # add the column names to the first line
+        for line in lines:
+            f.write(line)
+        f.close()
+            
 class Dataframe:
 
     def __init__(self, file_name, row_interval):
@@ -43,6 +92,32 @@ class Dataframe:
         df = df.drop(['index'], axis = 1)
         return df
     
+    def add_time_column(self, data_frame, hms):
+        '''call with dataframe object and specify if want time in hours
+        minutes or seconds by passing 'h', 'm' or 's' to hms parameter'''
+
+        times_list = [] #holder for the conversion of elapsed time to time in hours
+        A, B, C = 0, 0, 0 #Time conversion coefficients
+    
+        if hms == 'h':
+            A, B, C = 1, 1 / 60, 1 / 3600
+        elif hms == 'm':
+            A, B, C = 60 , 1, 1 / 60
+        elif hms == 's':
+            A, B, C = 3600, 60, 1
+        else:
+            return 'Error: hms should be string "h" or "m" or "s"'
+    
+        for row_number in range(len(data_frame)):
+            times_list.append(data_frame.loc[row_number, 'Elapsed Time'])
+            hours = int(times_list[row_number][3])
+            minutes = int(times_list[row_number][5:7])
+            seconds = int(times_list[row_number][8:10])
+            times_list[row_number] = (hours * A) + (minutes * B) + (seconds * C)
+
+        hours_column = pd.Series(times_list)
+        data_frame.insert(loc = 0, column = 'Time (hours)', value = hours_column)
+
 class Plot:
 
     def __init__(self, title, x_values, y_values):
@@ -61,11 +136,7 @@ class Plot:
         if show_plot:
             plt.show()
 
-test_csv = Csv('test_data.csv')
 
-discharge_data = Dataframe('discharge_2cells_clean.csv', 400)
 
-sixcells = Plot('Cell Voltages: Discharge on Cell 3 & Cell 5',
-                 'Time (hours)', ['Cell 1', 'Cell 2', 'Cell 3',
-                                  'Cell 4', 'Cell 5', 'Cell 6'])
+
 
