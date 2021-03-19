ef6ca2313552206c3062d0cf797fa6c8b76d49d2
Amir Farhadi <amirf147@gmail.com>
2021-03-19 16:46:58 +0200

    added lines to remove data outliers and irrelevant data

--- graph_csv.py
+++ graph_csv.py
@@ -10,7 +10,9 @@ dc_df = Dataframe('discharge_2cells_clean.csv', 400)
 cardf = Dataframe('5cell_fullspeed_clean.csv')
 burstdf = Dataframe('continuous_steering_busts_clean.csv')
 chargedf = Dataframe('charging_clean.csv')
-data4x2_1df = Dataframe('4x2_fullspeed+steering_clean.csv')
+data4x2_1df = Dataframe('4x2_fullspeed_clean.csv')
+data4x2_2df = Dataframe('4x2_fullspeed+stop_clean.csv')
+data4x2_3df = Dataframe('4x2_fullspeed+steering_clean.csv')
 
 # initial set up of pandas dataframe + remove unwanted columns
 df = dc_df.clean()
@@ -18,6 +20,9 @@ df2 = cardf.clean()
 df3 = burstdf.clean()
 df4 = chargedf.clean()
 df5 = data4x2_1df.clean()
+df6 = data4x2_2df.clean()
+df7 = data4x2_3df.clean()
+
 
 
 # add column with time elapsed either hrs, min, or sec
@@ -26,6 +31,15 @@ cardf.add_time_column(df2, 's')
 burstdf.add_time_column(df3, 's')
 chargedf.add_time_column(df4, 'm')
 data4x2_1df.add_time_column(df5, 'm')
+data4x2_2df.add_time_column(df6, 'm')
+data4x2_3df.add_time_column(df7, 'm')
+
+# remove some data outliers from dataframes
+df4 = df4.iloc[30:7090]
+df6 = df6[df6['Cell 4'] != 0]
+df6 = df6.iloc[53::]
+df7 = df7[df7['Cell 1'] != 0]
+
 
 # Choose which columns to be graphed
 
@@ -45,7 +59,15 @@ charge_plot = Plot('Cell Voltages: Charging',
                    'Time (minutes)', ['Cell 1', 'Cell 2', 'Cell 3',
                                       'Cell 4', 'Cell 5', 'Cell 6'])
 
-data4x2_1_plot = Plot('Cell Voltages: Fullspeed+steering+stops run with 8 cells (4x2)',
+data4x2_1_plot = Plot('Cell Voltages: Fullspeed run with 8 cells (4x2)',
+                   'Time (minutes)', ['Cell 1', 'Cell 2', 'Cell 3',
+                                      'Cell 4'])
+
+data4x2_2_plot = Plot('Cell Voltages: Fullspeed+stops run with 8 cells (4x2)',
+                   'Time (minutes)', ['Cell 1', 'Cell 2', 'Cell 3',
+                                      'Cell 4'])
+
+data4x2_3_plot = Plot('Cell Voltages: Fullspeed+steering+stops run with 8 cells (4x2)',
                    'Time (minutes)', ['Cell 1', 'Cell 2', 'Cell 3',
                                       'Cell 4'])
 #Apply plot setting and show plot
@@ -54,3 +76,5 @@ cardata_plot.set_options(df2, show_plot = True)
 burst_plot.set_options(df3, show_plot = True)
 charge_plot.set_options(df4, show_plot = True)
 data4x2_1_plot.set_options(df5, show_plot = True)
+data4x2_2_plot.set_options(df6, show_plot = True)
+data4x2_3_plot.set_options(df7, show_plot = True)
